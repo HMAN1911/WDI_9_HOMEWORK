3ae8f3c5c963748a7f62fa336e0172870a9b79e9
Tim Walter <twwalter@gmail.com>
2016-10-05 10:01:14 +1100

    hw update

---
+++ Tim_Walter/w2/dancing_cats/cats.js
@@ -0,0 +1,42 @@
+console.log('Dancing Cats...');
+
+var movePixels = 10;
+var delayMs = 50;
+var catTimer = null;
+var direction = 1;
+var currentLeft = 0;
+function catWalk() {
+  var img = document.getElementsByTagName('img')[0];
+  currentLeft = parseInt(img.style.left);
+  console.log(currentLeft);
+  img.style.left = (currentLeft + (movePixels*direction)) + 'px';
+  if (window.innerWidth / currentLeft > 2.4 && window.innerWidth / currentLeft < 2.45 ) {
+    movePixels = 0;
+  }
+  if (currentLeft > (window.innerWidth-img.width)) {
+    direction = -1;
+  } else if (currentLeft < 0) {
+    direction = 1;
+  }
+}
+function startCatWalk() {
+  catTimer = window.setInterval(catWalk, delayMs);
+}
+
+document.getElementById('start-button').addEventListener('click', function() {
+  movePixels = 10;
+  clearInterval(catTimer);
+  startCatWalk();
+});
+
+document.getElementById('speed-button').addEventListener('click', function() {
+  movePixels = 20;
+  clearInterval(catTimer);
+  startCatWalk();
+});
+
+document.getElementById('stop-button').addEventListener('click', function() {
+  movePixels = 0;
+  clearInterval(catTimer);
+  startCatWalk();
+});
