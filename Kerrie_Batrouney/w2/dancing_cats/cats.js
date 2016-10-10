3bf5363fa8667a1153ee669fcc687ab57167d33f
Kerrie Batrouney <batrouney@gmail.com>
2016-10-10 23:06:32 +1100

    latest versions of homework w2

--- Kerrie_Batrouney/w2/dancing_cats/cats.js
+++ Kerrie_Batrouney/w2/dancing_cats/cats.js
@@ -1,48 +1,69 @@
 var movePixels = 10;
 var delayMs = 50;
 var catTimer = null;
-var direction = 1;
+// var direction = 1;
+var goRight = true;
+
 function catWalk() {
   var img = document.getElementsByTagName('img')[0];
+
   var currentLeft = parseInt(img.style.left);
-  img.style.left = (currentLeft + movePixels*direction) + 'px';
+  if (goRight) {
+    img.style.left = (currentLeft + movePixels) + 'px';
+    // if in middle change cat to http://www.lovethisgif.com/uploaded_images/72438-Bengal-Chatter-The-Cat-s-Meow-In-Bengal-Cat-Forums.gif
+  }else{
+    img.style.left = (currentLeft - movePixels) + 'px';
+
+}
   if (currentLeft > (window.innerWidth-img.width)) {
-    //img.style.left = '0px';
-    direction = -1;
+    //flip cat
+    img.style.transform = 'scaleX(-1)';
+    goRight = false;
   }else if (currentLeft < 0) {
-    direction = 1;
+    img.style.transform = 'scaleX(1)';
+    goRight=true;
   }
-  }
+  // if in middle change cat to http://www.lovethisgif.com/uploaded_images/72438-Bengal-Chatter-The-Cat-s-Meow-In-Bengal-Cat-Forums.gif
+  if (currentLeft>= window.innerWidth/2) {
+    document.getElementById(catImg).src ="http://www.lovethisgif.com/uploaded_images/72438-Bengal-Chatter-The-Cat-s-Meow-In-Bengal-Cat-Forums.gif";
+//set timeout for dancing
+    setTimeout(stopCat, 1000);
+  document.getElementById(catImg).src="http://www.anniemation.com/clip_art/images/cat-walk.gif";
+
+}
+
+  // if in middle change cat to http://www.lovethisgif.com/uploaded_images/72438-Bengal-Chatter-The-Cat-s-Meow-In-Bengal-Cat-Forums.gif
+//   if (currentLeft>= window.innerWidth/2) {
+//     document.getElementById(catImg).src ="http://www.lovethisgif.com/uploaded_images/72438-Bengal-Chatter-The-Cat-s-Meow-In-Bengal-Cat-Forums.gif";
+// //set timeout for dancing
+//change back to old cat
 
 
+}
 
-// var direction = 1;
-// function catWalk() {
-//  var img = document.getElementsByTagName('img')[0];
-//  var currentLeft = parseInt(img.style.left);
-//  img.style.left = (currentLeft + (movePixels*direction)) + 'px';
-//  if (currentLeft > (window.innerWidth-img.width)) {
-//    direction = -1;
-//  } else if (currentLeft < 0) {
-//    direction = 1;
-//  }
-// }
 
 function startCatWalk() {
 movePixels = 10;
 //delayMs = 150;
   clearInterval(catTimer);
   catTimer = window.setInterval(catWalk, delayMs);
+  // if in middle change cat to http://www.lovethisgif.com/uploaded_images/72438-Bengal-Chatter-The-Cat-s-Meow-In-Bengal-Cat-Forums.gif
+//   if (currentLeft>= window.innerWidth/2) {
+//     document.getElementById(catImg).src ="http://www.lovethisgif.com/uploaded_images/72438-Bengal-Chatter-The-Cat-s-Meow-In-Bengal-Cat-Forums.gif";
+// //set timeout for dancing
+//     setTimeout(stopCat, 1000);
+//   document.getElementById(catImg).src="http://www.anniemation.com/clip_art/images/cat-walk.gif";
+//
+// }
+
 }
 
 document.getElementById('start-button').addEventListener('click', startCatWalk);
 
 
-
-
 function speedUpCatWalk() {
-movePixels = 20;
-//delayMs = 50;
+  movePixels = 20;
+  //delayMs = 50;
   clearInterval(catTimer);
   catTimer = window.setInterval(catWalk, delayMs);
 }
