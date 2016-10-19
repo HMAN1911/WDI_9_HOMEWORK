25a7bdb8b156d6aa4831fa4206f0b74f8126324f
Micah Boyd <micahhope@yahoo.com>
2016-10-20 10:16:39 +1100

    completed 80% of happitails assignment, still need to have client put pet up for adoption

---
+++ Micah_Boyd/week4/happi_trails/client.rb
@@ -0,0 +1,48 @@
+
+require 'pry'
+
+# - Client:
+#   - A client should have a name.
+#   - A client should have a number of children.
+#   - A client should have an age.
+#   - A client should have a list of pets.
+
+class Client
+
+  def initialize(name)
+    @name = name
+  end
+
+  def client_name
+    @name
+  end
+
+  def num_children(children)
+    @children = children
+  end
+
+  def client_age(age)
+    @age = age
+  end
+
+  def client_pets(pets)
+    @pets = pets.split(', ')
+  end
+
+  def pets
+    @pets
+  end
+
+  def client_overview
+    puts '--- Client overview: ---'
+    puts "     Name: ... #{@name}"
+    puts " Children: ... #{@children}"
+    puts "      Age: ... #{@age}"
+    puts "     Pets: ... #{@pets}"
+    puts ''
+  end
+
+
+end
+
+# binding.pry
