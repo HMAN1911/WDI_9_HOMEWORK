ea8950e926482e52792140fc20a1d9a808e63bb0
Micah Boyd <micahhope@yahoo.com>
2016-10-22 20:45:01 +1100

    added thumbs up feature. Need to fix thums down.

--- Micah_Boyd/week4/movie_search/main.rb
+++ Micah_Boyd/week4/movie_search/main.rb
@@ -11,5 +11,27 @@ get '/result' do
 @search_result = params[:search_input].downcase
 @result = HTTParty.get("http://omdbapi.com/?t=#{@search_result}")
 @poster = @result['Poster']
+@meta_thumb = nil
+@imdb_thumb = nil
+@meta_score = @result['Metascore'].to_i
+@imdb_score = @result['imdbRating'].to_f
+@thumb_up = 'Hands-Thumb-Up-icon.png'
+@thumb_down = 'Hands-Thumb-Up-icon.png'
+
+if @meta_score >= 70
+  @meta_thumb = @thumb_up.to_s
+end
+if @meta_score <= 60
+ @meta_thumb = @thumb_down.to_s
+end
+
+if @imdb_score > 7
+  @imdb_thumb = @thumb_up.to_s
+end
+if @imdb_score < 6
+  @imdb_thumb = @thumb_down.to_s
+end
+
+
   erb :result
 end
