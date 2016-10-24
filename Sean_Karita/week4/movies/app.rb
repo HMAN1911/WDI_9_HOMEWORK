d0a2f9acacc3cad53267a98f9bd5c2106020f5b4
Shoshiro <skarita@gmail.com>
2016-10-25 01:12:12 +1100

    updated movies

--- Sean_Karita/week4/movies/app.rb
+++ Sean_Karita/week4/movies/app.rb
@@ -4,17 +4,31 @@ require 'httparty'
 
 
 get '/' do
-  if params.length != 0
+  erb :index
+end
+
+get '/about' do
+  @movie_name = params[:movie]
+  if @movie_name != nil
     # if params[:movies].length != 0
-      @movie_name = params[:movie]
       @result = HTTParty.get('http://omdbapi.com/?t=' + @movie_name);
       @title = @result['Title']
       @year = @result['Year']
       @rated = @result['Rated']
       @poster = @result['Poster']
+      @runtime = @result['Runtime']
+      @genre = @result['Genre']
+      @director = @result['Director']
+      @writer = @result['Writer']
+      @actor = @result['Actors']
+      @plot = @result['Plot']
+      @country = @result['Country']
+      @imdb = @result['imdbRating']
+      @metascore = @result['Metascore']
+
     # end
   end
-  erb :index
+  erb :about
 end
 
 # get 'index' do
