4a008e952d35c659a2712a07ef32e36f6483c877
Catherine Gracey <catherine.gracey@gmail.com>
2016-10-24 16:18:08 +1100

    Copied from week 4. Searches movies and displays data if only one movie is found.

---
+++ Catherine_Gracey/w5/movies/app.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+
+get '/' do
+  if params.length > 0
+    if params[:search]
+      search = "http://omdbapi.com/?s=#{params[:search]}"
+      @result = HTTParty.get(search)
+      p @result
+      if @result["totalResults"] == "1"
+        imdbID = @result["Search"][0]["imdbID"]
+        search = "http://omdbapi.com/?i=#{imdbID}"
+        @result = HTTParty.get(search)
+      end
+    end
+    if params[:title]
+      imdbID = params[:title]
+      search = "http://omdbapi.com/?i=#{imdbID}"
+      @result = HTTParty.get(search)
+    end
+    p @result
+  else
+    @result = nil
+  end
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
