43d6f634c12f626aa279e0b7b793ac5bc00fb53c
Pedro Araujo <ppteixeira@outlook.com>
2016-10-25 02:10:43 +1100

    Week 5 Day 1 - Movies

--- Pedro_Araujo/week4/Movies/app.rb
+++ Pedro_Araujo/week4/Movies/app.rb
@@ -2,13 +2,36 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 require 'httparty'
+require 'uri'
 
 
 get '/' do
-  @movie = HTTParty.get("http://omdbapi.com/?t=#{params[:search_title]}")
+
   erb :index
+end
+get '/search' do
+  @search = HTTParty.get("http://omdbapi.com/?s=#{params[:search_title]}")
+
+  if @search['Search'] == nil
+    redirect to "/error"
+  elsif @search['Search'].count != nil && @search['Search'].count == 1
+    redirect to "/result?search_title=#{params[:search_title]}"
+  else
+    @list = @search['Search']
+  end
+
+  erb :search
+end
+
+get '/result' do
+  @movie = HTTParty.get("http://omdbapi.com/?t=#{params[:search_title]}")
+  erb :result
 end
 
 get '/about' do
   erb :about
+end
+
+get '/error' do
+  erb :error
 end
