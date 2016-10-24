2192645d4b1928f8302d7ef86181743c4bfb6110
CKS <ckseng86@gmail.com>
2016-10-24 16:45:00 +1100

    add new find page for multiple results

--- Ka_Seng_Chan/w4/d5/app.rb
+++ Ka_Seng_Chan/w4/d5/app.rb
@@ -12,12 +12,24 @@ get '/about' do
   if movie.strip == ''
     redirect '/'
   else
-    link = 'http://omdbapi.com/?t=' + movie
+    link = 'http://www.omdbapi.com/?t=' + movie
     @movie_data = HTTParty.get(link)
     if @movie_data["Response"] == "False"
       redirect '/'
     else
       erb :about
     end
+  end
+end
+
+get '/find' do
+  @movie = params[:movie].strip
+  @results = HTTParty.get('http://www.omdbapi.com/?s=' + @movie)
+  if @results["Response"] == "False"
+    erb :find
+  elsif @results['Search'].length == 1
+    redirect "/about?movie=#{@results['Search'][0]['Title']}"
+  elsif @results['Search'].length > 1
+    erb :find
   end
 end
