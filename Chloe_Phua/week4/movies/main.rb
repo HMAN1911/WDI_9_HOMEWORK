a606423061cb2333dd6ac23bed8023214a8336dd
Chloe Phua <cqphua@gmail.com>
2016-10-24 18:49:26 +1100

    add search results where multiple titles for searched entry exist and history file

--- Chloe_Phua/week4/movies/main.rb
+++ Chloe_Phua/week4/movies/main.rb
@@ -7,12 +7,42 @@ get '/' do
   erb :index
 end
 
+post '/search' do
+  @list = HTTParty.get('http://omdbapi.com/?s=' + params[:search])
+
+  @all_results = [];
+
+  @list["Search"].each_index do |result|
+    @result = @list["Search"][result]["Title"]
+    @all_results << @result
+  end
+
+  if @all_results.size == 1
+    redirect to "/about?title=#{@result}"
+  end
+
+  erb :search
+end
+
 get '/about' do
   @info = HTTParty.get('http://omdbapi.com/?t=' + params[:title])
-  # @year = HTTParty.get('http://omdbapi.com/?t=' + params[:year])
   @title = @info["Title"]
-  @year = @info["Year"]
   @poster = @info["Poster"]
 
+
+  open('search_history.txt', 'a') do |f|
+    f.puts "#{@title}"
+  end
+
+
   erb :about
+end
+
+get '/history' do
+  File.open("search_history.txt", "r") do |f|
+    f.each_line do |line|
+      puts line
+    end
+  end
+
 end
