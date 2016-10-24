3a7d295bcebdc272242cee8bbd59aeb03bf7cb37
Micah Boyd <micahhope@yahoo.com>
2016-10-25 09:03:18 +1100

    added list of movies search, linked it to detailed film page

--- Micah_Boyd/week4/movie_search/main.rb
+++ Micah_Boyd/week4/movie_search/main.rb
@@ -7,16 +7,29 @@ get '/' do
   erb :search
 end
 
+
 get '/result' do
 
-@search_result = params[:search_input].downcase
-@result = HTTParty.get("http://omdbapi.com/?t=#{@search_result}")
+  # /result?search_input=terminator&year=1981
 
-@poster = @result['Poster']
+  @search_result = params[:search_input].downcase
+  @result = HTTParty.get("http://omdbapi.com/?s=#{@search_result}")
+  @movies = @result['Search']
+  erb :result
+
+end
+
+get '/detail' do
+
+@imdbID = params[:imdbID]
+@detail = HTTParty.get("http://omdbapi.com/?i=#{@imdbID}")
+
+
+@poster = @detail['Poster']
 @meta_thumb = false
 @imdb_thumb = false
-@meta_score = @result['Metascore'].to_i
-@imdb_score = @result['imdbRating'].to_f
+@meta_score = @detail['Metascore'].to_i
+@imdb_score = @detail['imdbRating'].to_f
 @thumb_up = 'Hands-Thumb-Up-icon.png'
 @thumb_down = 'Hands-Thumb-Down-icon.png'
 
@@ -42,6 +55,5 @@ if @imdb_score == 0
   @imdb_score = false
 end
 
-# binding.pry
-  erb :result
+erb :detail
 end
