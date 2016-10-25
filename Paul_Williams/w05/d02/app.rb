7a42099f96e933fd6fb2b02e5a29977ab731895c
Paul Williams <paul@pdwilliams.net>
2016-10-25 16:58:53 +1100

    display new recipe form

--- Paul_Williams/w05/d02/app.rb
+++ Paul_Williams/w05/d02/app.rb
@@ -15,7 +15,11 @@ get '/' do
   erb :index
 end
 
-get '/:id' do
+get '/recipe/:id' do
   @recipe = run_sql("SELECT * FROM recipes WHERE id = #{ params[:id] };")[0]
   erb :recipe_details
+end
+
+get '/new' do
+  erb :new_recipe
 end
