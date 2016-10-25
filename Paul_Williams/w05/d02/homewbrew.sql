73c58e8a3f974d3ec90de568448f689e098817b6
Paul Williams <paul@pdwilliams.net>
2016-10-25 16:28:36 +1100

    db schema

---
+++ Paul_Williams/w05/d02/homewbrew.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE homebrew;
+
+CREATE TABLE recipes (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(200),
+  style VARCHAR(200),
+  grain_bill TEXT,
+  mash_temp INT,
+  mash_time INT,
+  hop_additions TEXT,
+  ferment_temp INT,
+  OSG REAL,
+  FSG REAL,
+  ABV VARCHAR(5),
+  comments TEXT
+);
+
+INSERT INTO recipes (name, style, grain_bill, mash_temp, mash_time, hop_additions, ferment_temp, OSG, FSG, ABV) VALUES ('Brunswick Pale', 'American Pale Ale', 'Maris Otter Pilsner - 1kg, Crystal - 200g', 67, 60, '15g Cascade @ 60min, 15g Cascade @ 30min, 10g Citra @ 0min', 19, 1.023, 1.010, '5.0%');
