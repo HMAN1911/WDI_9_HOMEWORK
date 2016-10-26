42ae083518c5139bd3328dd28519abb9c202b3c4
bruno <bruno.rampinelli.rota@gmail.com>
2016-10-27 08:32:18 +1100

    Improvements to add players

--- Bruno_Rampinelli/week5/day2/footballplayers.sql
+++ Bruno_Rampinelli/week5/day2/footballplayers.sql
@@ -10,3 +10,12 @@ CREATE TABLE players (
 
 INSERT INTO players (name, image_url, position, nationality) VALUES
 ('Roberto Baggio', 'http://www.ilciriaco.it/images/Roberto_Baggio_Brescia.jpg', 'number 10', 'Italian');
+
+INSERT INTO players (name, image_url, position, nationality) VALUES
+('Andrea Pirlo', 'http://golcalcio.it/pirlobr1.jpg', 'midfielder', 'Italian');
+
+INSERT INTO players (name, image_url, position, nationality) VALUES
+('Matteo Sereni', 'http://nst.sky.it/immagini/sport/calcio_italiano/2015/06/30/original/matteo_sereni_getty.jpg', 'goalkeeper', 'Italian');
+
+INSERT INTO players (name, image_url, position, nationality) VALUES
+('Gilberto Martinez', 'http://www.tuttocalciatori.net/fotocalciatori/Gilberto%20Martinez%20Vidal.jpg', 'Right Back', 'Costa Rica');
--- Bruno_Rampinelli/week5/day2/main.rb
+++ Bruno_Rampinelli/week5/day2/main.rb
@@ -18,3 +18,14 @@ get '/' do
   @players = run_sql("SELECT * from players;")
   erb :index
 end
+
+get '/players/new' do
+
+  erb :players_new
+end
+
+post '/players' do
+
+  @players  = run_sql("INSERT INTO players (name, image_url, position, nationality) VALUES ( '#{params[:name]}', '#{params[:image_url]}', '#{params[:position]}' , '#{params[:nationality]}');")
+  redirect '/'
+end
