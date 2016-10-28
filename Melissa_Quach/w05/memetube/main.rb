7a86954a24b8de7b6d1afe37e8c05d79f29ef788
PakkuDon <pakku.pd@gmail.com>
2016-10-28 16:44:29 +1100

    Add video submission form

--- Melissa_Quach/w05/memetube/main.rb
+++ Melissa_Quach/w05/memetube/main.rb
@@ -3,7 +3,33 @@ require 'sinatra/reloader'
 require_relative 'db_config'
 require_relative 'models/video'
 
+# Show all videos
 get '/' do
   @videos = Video.all
   erb :index
 end
+
+# Show a single video
+
+# Show add video form
+get '/videos/new' do
+  erb :video_new
+end
+
+# Add video to database
+post '/videos' do
+  video = Video.new
+  video.title = params[:title]
+  video.description = params[:description]
+  video.url = params[:url]
+  video.genre = params[:genre]
+
+  video.save
+  redirect to '/'
+end
+
+# Show edit form
+
+# Update video details
+
+# Delete video
