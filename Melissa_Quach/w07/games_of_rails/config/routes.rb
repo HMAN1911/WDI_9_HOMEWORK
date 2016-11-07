578ea95e43a5039d83e0814e5e8c167ae4d7851a
PakkuDon <pakku.pd@gmail.com>
2016-11-07 15:54:41 +1100

    Add eight ball action

--- Melissa_Quach/w07/games_of_rails/config/routes.rb
+++ Melissa_Quach/w07/games_of_rails/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   get '/games', to: 'games#index'
   get '/games/eight_ball', to: 'games#eight_ball'
-  get '/games/eight_ball/:question', to: 'games#eight_ball_result'
   get '/games/secret_number', to: 'games#secret_number'
-  get '/games/secret_number/:guess', to: 'games#secret_number_result'
   get '/games/rock_paper_scissors/', to: 'games#rock_paper_scissors'
   get '/games/rock_paper_scissors/:throw', to: 'games#rock_paper_scissors_result'
 end
