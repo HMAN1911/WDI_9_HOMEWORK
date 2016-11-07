55af102885f494e9baee48e29e34a3a8690dbdcc
Tim Walter <twwalter@gmail.com>
2016-11-07 18:11:12 +1100

    w7 game of rails hw initial commit

---
+++ Tim_Walter/w7/game_of_rails/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #get url to viewfoler / page
+  get '/', to: 'magic#index'
+
+  get '/magic', to: 'magic#index'
+  post '/get_answer', to: 'magic#get_answer'
+
+  get '/secret_num', to: 'secret_num#index'
+  post '/guess', to: 'secret_num#guess'
+  get '/win', to: 'secret_num#win'
+
+  get '/rock_paper_scissors', to: 'rock_paper_scissors#index'
+
+
+end
