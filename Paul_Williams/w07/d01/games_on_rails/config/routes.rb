8328cbda96da8747e10c4199a0f80c056dd05ab6
Paul Williams <paul@pdwilliams.net>
2016-11-07 22:12:03 +1100

    secret number game

--- Paul_Williams/w07/d01/games_on_rails/config/routes.rb
+++ Paul_Williams/w07/d01/games_on_rails/config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get '/8ball', to: 'eight_ball#index'
   post '/8ball', to: 'eight_ball#answer'
 
+  get '/secret_number', to: 'secret_number#index'
+  post '/secret_number', to: 'secret_number#answer'
+
 end
