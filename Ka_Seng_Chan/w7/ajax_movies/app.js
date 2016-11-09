6cbf603d0620ca7c148b612d32aa9ab24903ad54
CKS <ckseng86@gmail.com>
2016-11-09 21:14:53 +1100

    add ajax movies homework w7d3

---
+++ Ka_Seng_Chan/w7/ajax_movies/app.js
@@ -0,0 +1,22 @@
+
+$('.search-btn').on('click', function() {
+
+  $movieTitle = $('.search-input').val();
+  // console.log($movieTitle);
+  event.preventDefault();
+  var options = {
+    url: 'http://www.omdbapi.com/?s=' + $movieTitle,
+    method: 'post',
+    data: $movieTitle
+  }
+  function updateSearchResults(response) {
+    // console.log(response);
+    $('.search-results').empty();
+    var moviesSearch = response['Search'];
+    // console.log(response['Search']);
+    for (var i = 0; i < moviesSearch.length; i++) {
+      $('.search-results').append('<h2><a href="http://www.imdb.com/title/' + moviesSearch[i]['imdbID'] + '">'  + moviesSearch[i]['Title'] + '</a></h2>');
+    }
+  }
+  $.ajax(options).done(updateSearchResults);
+})
