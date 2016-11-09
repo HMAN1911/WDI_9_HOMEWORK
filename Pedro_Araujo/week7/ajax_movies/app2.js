d8b658dcacf955032d2b5bafd29043c6f38f1548
Pedro Araujo <ppteixeira@outlook.com>
2016-11-10 00:50:41 +1100

    HW week 7 day 3 - Movies_Ajax

---
+++ Pedro_Araujo/week7/ajax_movies/app2.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $("#btn-search").click(function() {
+      debugger
+      event.preventDefault();  // Prevent the button to send the
+      console.log('test');
+  });
+});
