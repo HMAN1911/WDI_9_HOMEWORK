6f274e1f4c08d2e44454bdb0aa19b30b8d55f551
Catherine Gracey <catherine.gracey@gmail.com>
2016-11-09 22:00:19 +1100

    Update to characters schema

--- Catherine_Gracey/w5/characters/storycharacters.sql
+++ Catherine_Gracey/w5/characters/storycharacters.sql
@@ -1,11 +1,22 @@
 CREATE DATABASE storycharacters;
 
+CREATE TABLE users (
+  id SERIAL4 PRIMARY KEY,
+  username VARCHAR(400),
+  email VARCHAR(200),
+  password_digest VARCHAR(400)
+);
+
 CREATE TABLE characters (
   id SERIAL4 PRIMARY KEY,
+  user_id INTEGER,
   name VARCHAR(400),
   gender VARCHAR(30),
-  age INTEGER
+  age INTEGER,
+  visible BOOLEAN
 );
+
+ALTER TABLE characters ADD visible BOOLEAN;
 
 INSERT INTO characters (name, gender, age)
 VALUES ('Vladamir the Timid', 'male', '25');
