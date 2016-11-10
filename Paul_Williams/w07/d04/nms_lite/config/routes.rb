83fcb69ccf4e44f5e41291815a6dd6e10a2b89d3
Paul Williams <paul@pdwilliams.net>
2016-11-10 17:59:30 +1100

    show individual planet

--- Paul_Williams/w07/d04/nms_lite/config/routes.rb
+++ Paul_Williams/w07/d04/nms_lite/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get '/', to: 'planets#index'
+  get '/planets/:id', to: 'planets#show'
 
   get '/login', to: 'session#new'
   post '/session', to: 'session#create'
