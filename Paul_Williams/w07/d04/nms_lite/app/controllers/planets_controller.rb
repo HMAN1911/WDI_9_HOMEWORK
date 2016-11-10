05794be76453458a056dce2377572d0e40c85ae6
Paul Williams <paul@pdwilliams.net>
2016-11-10 18:23:14 +1100

    new planet form

--- Paul_Williams/w07/d04/nms_lite/app/controllers/planets_controller.rb
+++ Paul_Williams/w07/d04/nms_lite/app/controllers/planets_controller.rb
@@ -3,6 +3,9 @@ class PlanetsController < ApplicationController
     @planets = Planet.all
   end
 
+  def new
+  end
+
   def show
     @planet = Planet.find params[:id]
     @moons = @planet.moons
--- Paul_Williams/w07/d04/nms_lite/config/routes.rb
+++ Paul_Williams/w07/d04/nms_lite/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   get '/', to: 'planets#index'
+  get '/planets/new', to: 'planets#new'
   get '/planets/:id', to: 'planets#show'
+  post '/planets', to: 'planets#create'
 
   get '/login', to: 'session#new'
   post '/session', to: 'session#create'
