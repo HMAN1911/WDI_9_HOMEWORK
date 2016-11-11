faeed3f75f9fd683fd15370abdaef11a4fdded18
bruno <bruno.rampinelli.rota@gmail.com>
2016-11-11 14:08:37 +1100

    Planets management almost done

--- Bruno_Rampinelli/week7/day3/app.js
+++ Bruno_Rampinelli/week7/day3/app.js
@@ -21,19 +21,50 @@ $('button').on('click',function(event) {
 
   console.log(event.target);
 
-  var $movie = $('#search').val();
+  var movie = $('#search').val();
   var options = {
-    url: 'http://omdbapi.com/?t=' + $movie,
-    method: 'get',
-    data: { movie_title: $movie }
+    url: 'http://omdbapi.com/?s=' + movie,
+    method: 'get'
+    // data: { movie_title: movie }
   }
 
   var update = function(data) {
     console.log ("Hello")
     console.log(data)
-    var $search = $(event.target).closest('.search');
+    $("#data").text(data.Title)
+
   }
 
   $.ajax(options).done(update)
 
 });
+
+
+PEDRO SOLUTION
+
+$( "#btnsearch" ).click(function(event) {
+
+    event.preventDefault();  // Prevent the button to send the
+    var search = $("#search-box").val(); // #search-box is the text input
+    var url_s = "http://omdbapi.com/?s=" + search; //url that searches by title
+
+    var options = {
+      method: 'GET',
+      url: url_s,
+      dataType: 'json',
+      success: function(results){
+        for (var i = 0; i < results.Search.length; i++) {
+          var url_id = "http://omdbapi.com/?i=" + results.Search[i].imdbID; //url that searches by imdbID
+          var item = "<h2> <a href='"+ url_id + "' >" + results.Search[i].Title + "</a></h2>"; // each item created
+          $(item).appendTo( $(".container") ); // add item to container div
+        }
+      },
+      error: function(error){
+        console.log("@ERROR", error);
+      }
+
+    };
+
+    $.ajax(options);
+    $("#search-box").val('') // clean the input
+  });
