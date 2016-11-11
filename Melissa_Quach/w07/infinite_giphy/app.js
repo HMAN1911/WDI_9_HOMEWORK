de2b3690aa7d1bb593a50bca35ac72a11cc3b59b
PakkuDon <pakku.pd@gmail.com>
2016-11-11 16:55:46 +1100

    Add scroll pagination

--- Melissa_Quach/w07/infinite_giphy/app.js
+++ Melissa_Quach/w07/infinite_giphy/app.js
@@ -4,6 +4,37 @@ var $input = $('#q');
 
 var offset = 0;
 
+// Retrieve search results from Giphy API and pass results to callback
+var loadGiphyResults = function(searchTerm) {
+  $.ajax({
+    url: 'http://api.giphy.com/v1/gifs/search',
+    data: {
+      q: searchTerm,
+      api_key: 'dc6zaTOxFJmzC',
+      offset: offset
+    }
+  })
+  .done(function(res) {
+    offset += res.pagination.count;
+    $grid.append(generateThumbnails(res.data));
+  });
+}
+
+// Convert search results to HTMl elements
+var generateThumbnails = function(posts) {
+  return posts.map(function(post) {
+    var $post = $('<a>', {
+      href: post.url,
+      class: 'grid-item',
+      target: '_blank'
+    });
+    $post.append($('<img>', {
+      src: post.images.downsized.url
+    }));
+    return $post;
+  });
+}
+
 // Retrieve images from Giphy
 $('form').submit(function(e) {
   e.preventDefault();
@@ -13,33 +44,12 @@ $('form').submit(function(e) {
   offset = 0;
 
   // Load images from Giphy API
-  $.ajax({
-    url: 'http://api.giphy.com/v1/gifs/search',
-    data: {
-      q: $input.val(),
-      api_key: 'dc6zaTOxFJmzC',
-      offset: offset
-    }
-  })
-  .done(function(res) {
-    offset += res.pagination.count;
-    var $images = res.data.map(function(post) {
-      var $post = $('<a>', {
-        href: post.url,
-        class: 'grid-item'
-      });
-      $post.append($('<img>', {
-        src: post.images.downsized.url
-      }));
-      return $post;
-    });
-
-    $grid.append($images);
-    console.log(res);
-  });
+  loadGiphyResults($input.val());
 });
 
 // Retrieve more images from Giphy once user reaches bottom of screen
 $(window).scroll(function() {
-
+  if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+    loadGiphyResults($input.val());
+  }
 });
