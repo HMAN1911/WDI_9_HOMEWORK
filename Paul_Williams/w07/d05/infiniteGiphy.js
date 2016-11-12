9bc3c0046de91fbb54166c1ca253c7f096f634d8
Paul Williams <paul@pdwilliams.net>
2016-11-12 16:04:33 +1100

    display more gifs on scroll

--- Paul_Williams/w07/d05/infiniteGiphy.js
+++ Paul_Williams/w07/d05/infiniteGiphy.js
@@ -1,6 +1,14 @@
-var $input = $('input');
-var $button = $('button');
 var $body = $('body');
+var $window = $(window);
+var $document = $(document);
+var $input = $('input');
+
+var offset = 0;
+var query = '';
+
+var clearResults = function() {
+  $('img').remove();
+}
 
 var drawResult = function(i, v) {
   var url = v.images.fixed_height.url;
@@ -8,14 +16,29 @@ var drawResult = function(i, v) {
   $body.append($img);
 }
 
-var getFirstTenResults = function() {
-  var request = $.get('http://api.giphy.com/v1/gifs/search?q=' + $input.val() + '&limit=10&api_key=dc6zaTOxFJmzC');
+var getNextTenResults = function() {
+  var request = $.get('http://api.giphy.com/v1/gifs/search?q=' + query + '&offset=' + offset + '&limit=10&api_key=dc6zaTOxFJmzC');
+  offset += 10;
   request.done(function(v) {
     $.each(v.data, drawResult);
   });
 }
 
-$button.click(getFirstTenResults)
+var getInitialResults = function() {
+  clearResults();
+  offset = 0;
+  query = $input.val();
+  getNextTenResults();
+}
+
+var checkScrollPosition = function() {
+  if ($window.scrollTop() + $window.height() === $document.height()) {
+    getNextTenResults();
+  }
+}
+
+$('button').click(getInitialResults)
+$window.scroll(checkScrollPosition);
 
 /*
 # 2. Infinite Giphy
