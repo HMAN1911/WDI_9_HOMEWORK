2869e4350a4c93c939a87710cac7ec05807290d4
CKS <ckseng86@gmail.com>
2016-11-13 17:13:27 +1100

    add function to show more results when scroll to the bottom

--- Ka_Seng_Chan/w7/infinite_giphy/main.js
+++ Ka_Seng_Chan/w7/infinite_giphy/main.js
@@ -1,4 +1,4 @@
-console.log('its working')
+// console.log('its working')
 
 var $q = $('#q');
 var $resultsDIv = $('#search-results');
@@ -6,33 +6,42 @@ var limit = 10;
 var offset = 0;
 var apiKey = 'dc6zaTOxFJmzC';
 
+// function to show 10 results
+var showResults = function(response) {
+  // append 10 giphy images to div
+  var gifs = response.data;
+  for (var i = 0; i < gifs.length; i++) {
+    var newImg = '<img src="' + gifs[i].url + '" alt="">'
+    $('#search-results').append(newImg);
+  }
+}
+
 
 $('#search-btn').on('click', function(event) {
   event.preventDefault();
 
-  console.log($q.val());
-
   var options = {
     url: 'http://api.giphy.com/v1/gifs/search',
     method: 'get',
-    data_type: 'json',
+    // data_type: 'json',
     data: { q: $q, limit: limit, offset: offset, api_key: apiKey }
   }
 
-  var showResults = function(response) {
-    // append 10 giphy images to div
-    var gifs = response.data;
-    for (var i = 0; i < gifs.length; i++) {
-      var newImg = '<img src="' + gifs[i].url + '" alt="">'
-      $('#search-results').append(newImg);
-    }
-  }
-
   $.ajax(options).done(showResults);
+
 })
+
 
 $(window).scroll( function() {
   if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
-    
+    offset += 10;
+    var options = {
+      url: 'http://api.giphy.com/v1/gifs/search',
+      method: 'get',
+      // data_type: 'json',
+      data: { q: $q, limit: limit, offset: offset, api_key: apiKey }
+    }
+
+    $.ajax(options).done(showResults);
   }
 })
