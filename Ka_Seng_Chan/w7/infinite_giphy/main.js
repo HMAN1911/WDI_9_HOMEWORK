f896b04a1fb40381c30b04764220f8b756cc6de9
CKS <ckseng86@gmail.com>
2016-11-13 14:21:32 +1100

    add giphy api and showresults function

--- Ka_Seng_Chan/w7/infinite_giphy/main.js
+++ Ka_Seng_Chan/w7/infinite_giphy/main.js
@@ -1,7 +1,11 @@
 console.log('its working')
 
-$q = $('#q');
-$resultsDIv = $('#search-results');
+var $q = $('#q');
+var $resultsDIv = $('#search-results');
+var limit = 10;
+var offset = 0;
+var apiKey = 'dc6zaTOxFJmzC';
+
 
 $('#search-btn').on('click', function(event) {
   event.preventDefault();
@@ -9,14 +13,26 @@ $('#search-btn').on('click', function(event) {
   console.log($q.val());
 
   var options = {
-    url: '',
-    method: '',
-    data: ''
+    url: 'http://api.giphy.com/v1/gifs/search',
+    method: 'get',
+    data_type: 'json',
+    data: { q: $q, limit: limit, offset: offset, api_key: apiKey }
   }
 
   var showResults = function(response) {
     // append 10 giphy images to div
+    var gifs = response.data;
+    for (var i = 0; i < gifs.length; i++) {
+      var newImg = '<img src="' + gifs[i].url + '" alt="">'
+      $('#search-results').append(newImg);
+    }
   }
 
-  // $.ajax(options).done(showResults);
+  $.ajax(options).done(showResults);
+})
+
+$(window).scroll( function() {
+  if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+    
+  }
 })
