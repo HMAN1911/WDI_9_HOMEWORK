8db1d5cc43d23944f1c27ae8eeebfb260022ef14
Kerrie Batrouney <batrouney@gmail.com>
2016-11-14 16:29:28 +1100

    scrolling of giphy

--- Kerrie_Batrouney/w7/giphy/giphy.js
+++ Kerrie_Batrouney/w7/giphy/giphy.js
@@ -14,34 +14,54 @@ $('#search-btn').on('click', function() {
   var input = $('#q').val();
   var offset = 0;
  // debugger;
+
   var settings = {
     url: "http://api.giphy.com/v1/gifs/search", // required
-    data: {q: "beagle", api_key: "dc6zaTOxFJmzC", limit: 10, offset: 0},
+    data: {q: input, api_key: "dc6zaTOxFJmzC", limit: 10, offset: offset},
     method: 'get',
     fmt: 'JSONP' // usually auto detected
   };
   //  debugger;
 console.log(settings);
-  // make an ajax request to omdb api
+
+  // var getResults = function(){
+  //   var giphys = res.data;
+  //   giphys.forEach(displayResults(giphy));
+  // }
+  //
+  // var displayResults = function (){
+  //   var $pic = $('<img>').attr('src', giphy.images.fixed_height.url);
+  //   $('#list').append($pic);
+  //   $('#list').append($('<br>'));
+  // }
+
+  // make an ajax request to giphy api, display 10 results
+
+
   $.ajax(settings).done(function(res) {
-
     var giphys = res.data;
-
     giphys.forEach(function(giphy){
-
-      var $pic = $('<img>').attr('src', giphy.embed_url);
-
-      // var $link = $('<a>')
-      //   .attr('target', '_blank')
-      //   .attr('href', 'http://www.imdb.com/title/' + movie.imdbID)
-
-      // $link.append($h2);
-
+      var $pic = $('<img>').attr('src', giphy.images.fixed_height.url);
       $('#list').append($pic);
-
-      // $row.appendTo('#list');
+      $('#list').append($('<br>'));
     });
-
   });
+
+//as user scrolls to bottom, reset the offset and get more results from database and display them.
+  $(window).scroll(function() {
+     if($(window).scrollTop() + $(window).height() == $(document).height()) {
+         settings.data.offset = settings.data.offset + 10;
+         $.ajax(settings).done(function(res) {
+           var giphys = res.data;
+           giphys.forEach(function(giphy){
+             var $pic = $('<img>').attr('src', giphy.images.fixed_height.url);
+             $('#list').append($pic);
+             $('#list').append($('<br>'));
+           });
+
+         });
+     }
+  });
+
 
 });
