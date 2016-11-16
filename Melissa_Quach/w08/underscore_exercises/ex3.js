3c7a20d74f4dbbae94ea77aa4449c26401e49dbf
PakkuDon <pakku.pd@gmail.com>
2016-11-16 16:19:20 +1100

    Add part 3 of Underscore exercises

--- Melissa_Quach/w08/underscore_exercises/ex3.js
+++ Melissa_Quach/w08/underscore_exercises/ex3.js
@@ -4,14 +4,24 @@ var uncompactedArr = [ "hello", false, NaN, undefined, "quantom bogo-sort" ];
 
 var arrToTransform = [[ "age", "location" ], [ NaN, undefined ]];
 
+console.log('Exercise 3');
+
 // - Create an array of every five numbers between 30 and 101
+console.log(_.range(30, 101, 5));
 
 // - Turn ` bumpyArr ` into one flat array (no nested arrays)
+console.log(_.flatten(bumpyArr));
 
 // - Remove all of the falsey elements in the ` uncompactedArr `
+console.log(_.compact(uncompactedArr));
 
 // - Find all of the unique elements in the following arrays - ` [ 1, 25, 100 ] `, ` [ 1, 14, 25 ] ` and ` 24, Infinity, -0 `
+console.log(_.union([ 1, 25, 100 ], [ 1, 14, 25 ],  [24, Infinity, -0]));
 
 // - Find the index of the first element in ` numbers ` that is over 7 and is even
+console.log(_.findIndex(numbers, function(number) {
+  return number > 7 && number % 2 == 0;
+}));
 
 // - Turn ` arrToTransform ` into an object that looks like this - ` { age: NaN, location: undefined } `
+console.log(_.object(arrToTransform[0], arrToTransform[1]));
