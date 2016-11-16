10531f49188edb07207f74b9747f22b848e5c0ac
Tim Walter <twwalter@gmail.com>
2016-11-16 14:02:53 +1100

    complete infinte giphy

--- Tim_Walter/w7/infinite_giphy/giphy.js
+++ Tim_Walter/w7/infinite_giphy/giphy.js
@@ -3,25 +3,43 @@ var api_key = "&api_key=dc6zaTOxFJmzC";
 var search_url;
 var limit = "&limit=10";
 var offset = "&offset=";
-var offset_val = 10;
+var offset_val = 0;;
 
 $( document ).ready(function() {
+  // get click event
   $(document).on('click', '#search_btn', function (e) {
     e.preventDefault();
-    search = $('#search_req').val().replace(" ", "+");
-    console.log(search);
-    search_url = giphy_api + search + api_key + limit + offset + offset_val;
-    console.log(search_url);
-
-    var options = {
-      url: search_url,
-      type: "GET"
-    }
-
-    $.ajax(options).done(showResults).fail(callFail);
-
+    offset_val = 0;
+    $('.giphy_results').empty();
+    doSearch();
   });
 });
+
+
+function doSearch() {
+  search = $('#search_req').val().replace(" ", "+");
+  search_url = giphy_api + search + api_key + limit + offset + offset_val;
+  var options = {
+    url: search_url,
+    type: "GET"
+  }
+  $.ajax(options).done(showResults).fail(callFail);
+}
+
+
+
+// scroll to bottom request 10 more
+$(window).scroll(function () {
+    var scroll_top = $(window).scrollTop();
+    var w_height = $(window).height();
+    var d_height = $(document).height();
+    if (scroll_top + w_height === d_height) {
+      offset_val += 10;
+      doSearch();
+      console.log('Loading 10 more');
+    }
+});
+
 
 
 function showResults(data) {
@@ -40,11 +58,6 @@ function showResults(data) {
     autoplay: true
     });
     video.appendTo($('.giphy_results'));
-
-
-
-
-
   }
 
 };
