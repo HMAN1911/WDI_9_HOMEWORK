cac71e7c1d664a1f730e2b73a1188d42b9b1ea5d
James Mah <junyeumah@gmail.com>
2016-11-28 17:06:31 +1100

    rgb to hex homework w10

---
+++ James_Mah/w10/browserify_ex1/app.js
@@ -0,0 +1,22 @@
+var camelCase = require('camelcase');
+console.log(camelCase('foo-bar'));
+
+var rgbHex = require('rgb-hex');
+
+$('.input-form').submit(function(event) {
+  event.preventDefault();
+
+  var input = function(name) {
+    return Number($('.input-form').find('input[name=' + name + ']').val());
+  };
+
+  var r = input("r");
+  var g = input("g");
+  var b = input("b");
+
+  var hex = rgbHex(r, g, b);
+
+  $('.result-text span').text(hex);
+
+  $('.result-color').css('backgroundColor', '#' + hex);
+});
---
+++ James_Mah/w10/browserify_ex1/gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var babel  = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
+
+gulp.task('browserify', function () {
+  return browserify('./app.js')
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('server', function() {
+  connect.server();
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./app.js', ['browserify']);
+});
+
+gulp.task('default', ['server', 'watch']);
