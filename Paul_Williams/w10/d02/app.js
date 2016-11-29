0e307646deb11b12cc9e0b7b97f233e00517b856
Paul Williams <paul@pdwilliams.net>
2016-11-29 22:55:13 +1100

    movie search and display info

--- Paul_Williams/w10/d02/app.js
+++ Paul_Williams/w10/d02/app.js
@@ -1,4 +1,5 @@
 var app = require('express')()
+var request = require('request')
 
 app.set('view engine', 'pug')
 
@@ -7,7 +8,19 @@ app.get('/', function(req, res) {
 })
 
 app.get('/search', function(req, res) {
-  res.render('search', req.query)
-}
+  request('http://www.omdbapi.com/?s=' + req.query.input, function(err, response, body) {
+    if (!err && response.statusCode == 200) {
+      res.render('search', JSON.parse(body))
+    }
+  })
+})
+
+app.get('/movie/:id', function(req, res) {
+  request('http://www.omdbapi.com/?i=' + req.params.id, function(err, response, body) {
+    if (!err && response.statusCode == 200) {
+      res.render('movie', JSON.parse(body))
+    }
+  })
+})
 
 app.listen(3030)
